5efe76ff18944cbd10fec161724be3eccd9e2102
leemoonhee <zipsy327@gmail.com>
2025-05-07 11:51:45 +0900

    first commit

---
+++ src/test/java/com/example/demo/Final502projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Final502projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

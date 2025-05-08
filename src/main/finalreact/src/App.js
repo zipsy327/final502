f79ae24115eb4961ad6c8abbb2e04e3864b944d9
leemoonhee <zipsy327@gmail.com>
2025-05-08 10:52:27 +0900

    다시

---
+++ src/main/finalreact/src/App.js
@@ -0,0 +1,14 @@
+import mainimg from './mainimage.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img alyt='' src={mainimg} 
+      style={{width:'400px'}}/>
+      <h2><b>React+SpringBoot 자동배포실습!!!</b></h2>
+    </div>
+  );
+}
+
+export default App;

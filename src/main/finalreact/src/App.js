15621c9d7e0f89dabc5fd6073be86eb6d6eb3a85
leemoonhee <zipsy327@gmail.com>
2025-05-08 12:02:47 +0900

    다시

--- src/main/finalreact/src/App.js
+++ src/main/finalreact/src/App.js
@@ -1,11 +1,16 @@
 import mainimg from './mainimage.png';
 import './App.css';
+import error from './error.png';
 
 function App() {
   return (
     <div className="App">
       <img alyt='' src={mainimg} 
       style={{width:'400px'}}/>
+      <br/><br/>
+      <img alyt='' src={error} 
+      style={{width:'400px'}}/>
+
       <h2><b>React+SpringBoot 자동배포실습!!!</b></h2>
     </div>
   );
